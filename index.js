e9cffe5953d778f03b49845ea0e0058a033da83b
thanhannnn11 <traffikmanpun0suy@hotmail.com>
2024-05-08 23:06:58 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-function maxArea(height) {
-  let maxArea = 0;
-  let left = 0;
-  let right = height.length - 1;
-  while (left < right) {
-    maxArea = Math.max(
-      maxArea,
-      Math.min(height[left], height[right]) * (right - left),
-    );
-    if (height[left] < height[right]) left++;
-    else right--;
+function singleNumber(nums) {
+  let result = 0;
+  for (const num of nums) {
+    result ^= num;
   }
-  return maxArea;
+  return result;
 }

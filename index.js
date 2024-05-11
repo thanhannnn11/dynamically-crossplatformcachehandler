7d7d59ce99fa5c539794233f3d1ef63fadd6c406
thanhannnn11 <traffikmanpun0suy@hotmail.com>
2024-05-11 07:03:27 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const head = (arr) => arr[0];
+function permute(nums) {
+  const result = [];
+  backtrack([]);
+  return result;
+  function backtrack(permutation) {
+    if (permutation.length === nums.length) {
+      result.push([...permutation]);
+      return;
+    }
+    for (const num of nums) {
+      if (!permutation.includes(num)) {
+        permutation.push(num);
+        backtrack(permutation);
+        permutation.pop();
+      }
+    }
+  }
+}
